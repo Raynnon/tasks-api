84d9c0235cbe43eea3444a3f93271e8965162162
Raynnon <assante.f@gmail.com>
2021-02-23 02:07:19 +0100

    added cors

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 require("./db/mongoose");
 
 const userRouter = require("./routers/userRouter");
@@ -7,6 +8,7 @@ const taskRouter = require("./routers/taskRouter");
 const app = express();
 const port = process.env.port || 4000;
 
+app.use(cors());
 app.use(express.json());
 
 app.use(userRouter);
